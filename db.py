c66ead374a51d11c9aa48ada6b5dbfaa3b4d9fec
EmTori <emily.houlihan@outlook.com>
2021-06-29 17:21:41 -0230

    working on db file

--- db.py
+++ db.py
@@ -1,9 +1,31 @@
+import csv
+
+def loadMoney():
+    playersMoney = []
+    with open("money.txt", "r", newline = "") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            item = [row[0]]
+            playersMoney.append(item)
+    return playersMoney
+
+def saveMoney(playersMoney):
+    with open("money.txt", "w", newline = "") as file:
+        writer = csv.writer(file)
+        writer.writerows(playersMoney)
+        
 def main():
+
+    playersMoney = loadMoney()
 
     again = "y"
     while again == "y":
 
-        again = input("Play again? (y/n): ")
+        print("Money: " + str(playersMoney))
+
+        playersBet = int(input("How much money do you want to bet?: "))
+
+        again = input("Bet again? (y/n): ")
 
 if __name__ == "__main__":
     main()
